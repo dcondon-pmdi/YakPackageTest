45278aea911885affbba39a53c0d323bb38fd19c
Dustin Condon <dcondon@pmdi.com>
2025-10-15 11:28:06 -0700

    Update assembly info and add more properties to csproj

--- YakPackageTest/Properties/AssemblyInfo.cs
+++ YakPackageTest/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using Rhino.PlugIns;
 
 // Plug-in Description Attributes - all of these are optional.
 // These will show in Rhino's option dialog, in the tab Plug-ins.
-[assembly: PlugInDescription(DescriptionType.Address, "")]
-[assembly: PlugInDescription(DescriptionType.Country, "")]
-[assembly: PlugInDescription(DescriptionType.Email, "")]
-[assembly: PlugInDescription(DescriptionType.Phone, "")]
-[assembly: PlugInDescription(DescriptionType.Fax, "")]
-[assembly: PlugInDescription(DescriptionType.Organization, "")]
-[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
-[assembly: PlugInDescription(DescriptionType.WebSite, "")]
+[assembly: PlugInDescription(DescriptionType.Address, "123 My Address")]
+[assembly: PlugInDescription(DescriptionType.Country, "My Country")]
+[assembly: PlugInDescription(DescriptionType.Email, "my@email.com")]
+[assembly: PlugInDescription(DescriptionType.Phone, "555-5555")]
+[assembly: PlugInDescription(DescriptionType.Fax, "555-5555")]
+[assembly: PlugInDescription(DescriptionType.Organization, "MyCompany")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "mywebsite.com/updates")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "mywebsite.com")]
 
 // Icons should be Windows .ico files and contain 32-bit images in the following sizes: 16, 24, 32, 48, and 256.
 [assembly: PlugInDescription(DescriptionType.Icon, "YakPackageTest.EmbeddedResources.plugin-utility.ico")]
